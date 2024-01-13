50fc02d83dc30e470a8aac9613344db2f0c64e8b
Lachi-Amine <ma.lachi@esi-sba.dz>
2024-01-13 13:38:01 +0100

    h
    h

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ let j= setInterval(() => {
     let h=new Date().getHours();
     let m=new Date().getMinutes();
     let s=new Date().getSeconds();
-console.log(h);
-console.log(m);
-console.log(s);
 document.getElementById("hour").innerHTML=h;
 document.getElementById("minute").innerHTML=m;
 document.getElementById("second").innerHTML=s;
